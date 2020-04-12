1dbf577f2fd8f237a469df63b465564b40d919ec
Dwayne Campogan <dcampogan@gmail.com>
2020-04-11 20:38:28 -0700

    Extracting Options to Config Objects

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: '' }
+  state = { lat: null, errorMessage: ''};
+  // ABOVE STATE INITIALIZATION REFACTORED FROM BELOW..
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
 
+  // good place to do data loading
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({ lat: position.coords.latitude })
+        this.setState({ lat: position.coords.latitude });
       },
       (err) => {
-        this.setState({ errorMessage: err.message})
+        this.setState({ errorMessage: err.message });
       }
     );
   }
 
+  // render gets called a lot, don't do data loading here
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    };
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat} </div>
-    };
-    return (
-      <div>Loading!</div>
-    )
-  };
-};
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
